8ee508487fbab2cac8e82003d88f7bff91d65281
Marc Nathaniel Valeros <marcnathanielvaleros>
2022-11-11 02:48:26 +0800

    add textfield validation

--- lib/screens/SignupScreen.dart
+++ lib/screens/SignupScreen.dart
@@ -14,125 +14,172 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
-  String validation_msg = '';
+  bool obscurePassword1 = true;
+  bool obscurePassword2 = true;
+  String validationMsg = "";
+
   final TextEditingController fnameController = TextEditingController();
   final TextEditingController lnameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController confirmpasswordController =
       TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  bool obscurePassword = true;
 
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.black,
-          image: DecorationImage(
-            image: const AssetImage("assets/signup_scrn.jpg"),
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(
-                Colors.black.withOpacity(0.3), BlendMode.dstATop),
-          ),
-        ),
-        child: Center(
-          child: SingleChildScrollView(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.black,
+              image: DecorationImage(
+                image: const AssetImage("assets/signup_scrn.jpg"),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.3), BlendMode.dstATop),
+              ),
+            ),
             child: Center(
-              child: Container(
-                width: width * .9,
-                child: Column(
-                  children: [
-                    Row(
+              child: SingleChildScrollView(
+                child: Center(
+                  child: Container(
+                    width: width * .9,
+                    child: Column(
                       children: [
-                        Expanded(
-                          child: CustomTextField(
-                              labelText: "First Name",
-                              hintText: "Enter First Name",
-                              controller: fnameController,
-                              textInputType: TextInputType.text),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: CustomTextField(
+                                labelText: "First Name",
+                                hintText: "Enter First Name",
+                                controller: fnameController,
+                                textInputType: TextInputType.text,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 20.0,
+                            ),
+                            Expanded(
+                              child: CustomTextField(
+                                labelText: "Last Name",
+                                hintText: "Enter Last Name",
+                                controller: lnameController,
+                                textInputType: TextInputType.text,
+                              ),
+                            )
+                          ],
                         ),
                         const SizedBox(
-                          width: 20.0,
+                          height: 20.0,
                         ),
-                        Expanded(
-                          child: CustomTextField(
-                              labelText: "Last Name",
-                              hintText: "Enter Last Name",
-                              controller: lnameController,
-                              textInputType: TextInputType.text),
+                        CustomTextField(
+                          labelText: "Email Address",
+                          hintText: "Enter Email Address",
+                          controller: emailController,
+                          textInputType: TextInputType.emailAddress,
+                        ),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        PasswordField(
+                            labelText: "Password",
+                            hintText: "Enter Password",
+                            obscureText: obscurePassword1,
+                            onTap: handleObscurePassword1,
+                            controller: passwordController),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        PasswordField(
+                            labelText: "Confirm Password",
+                            hintText: "Enter Password",
+                            obscureText: obscurePassword2,
+                            onTap: handleObscurePassword2,
+                            controller: confirmpasswordController),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        PrimaryButton(
+                          text: "Signup",
+                          iconData: Icons.app_registration,
+                          onPress: () {
+                            if (fnameController.text == "" ||
+                                lnameController.text == "" ||
+                                emailController.text == "" ||
+                                passwordController.text == "" ||
+                                confirmpasswordController.text == "") {
+                              setState(() {
+                                validationMsg = "Fill up the required fields";
+                              });
+                            } else if (!emailController.text.contains("@") ||
+                                !emailController.text.contains(".com")) {
+                              setState(() {
+                                validationMsg = "Enter a valid email";
+                              });
+                            } else if (passwordController.text !=
+                                confirmpasswordController.text) {
+                              setState(() {
+                                validationMsg =
+                                    "Password mismatch. Please try again";
+                              });
+                            } else {
+                              Navigator.pushReplacementNamed(
+                                  context, LoginScreen.routeName);
+                            }
+                          },
+                        ),
+                        Container(
+                          child: Center(
+                            child: GestureDetector(
+                              onTap: () {
+                                Navigator.pushReplacementNamed(
+                                    context, LoginScreen.routeName);
+                              },
+                              child: const Text(
+                                "Already have an account? Log in here.",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    height: 2,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.normal),
+                              ),
+                            ),
+                          ),
                         )
                       ],
                     ),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    CustomTextField(
-                        labelText: "Email Address",
-                        hintText: "Enter Email Address",
-                        controller: emailController,
-                        textInputType: TextInputType.emailAddress),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    PasswordField(
-                        labelText: "Password",
-                        hintText: "Enter Password",
-                        obscureText: obscurePassword,
-                        onTap: handleObscurePassword,
-                        controller: passwordController),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    PasswordField(
-                        labelText: "Confirm Password",
-                        hintText: "Enter Password",
-                        obscureText: obscurePassword,
-                        onTap: handleObscurePassword,
-                        controller: confirmpasswordController),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    PrimaryButton(
-                      text: "Signup",
-                      iconData: Icons.app_registration,
-                      onPress: () {
-                        Navigator.pushReplacementNamed(
-                            context, LoginScreen.routeName);
-                      },
-                    ),
-                    Container(
-                      child: Center(
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.pushReplacementNamed(
-                                context, LoginScreen.routeName);
-                          },
-                          child: const Text(
-                            "Already have an account? Log in here.",
-                            style: TextStyle(
-                                color: Colors.white,
-                                height: 2,
-                                fontSize: 16,
-                                fontWeight: FontWeight.normal),
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 150,
+              child: Text(
+                validationMsg,
+                style: const TextStyle(
+                    fontSize: 15, color: Color.fromARGB(255, 248, 7, 7)),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  handleObscurePassword() {
+  handleObscurePassword1() {
     setState(() {
-      obscurePassword = !obscurePassword;
+      obscurePassword1 = !obscurePassword1;
+    });
+  }
+
+  handleObscurePassword2() {
+    setState(() {
+      obscurePassword2 = !obscurePassword2;
     });
   }
 }
